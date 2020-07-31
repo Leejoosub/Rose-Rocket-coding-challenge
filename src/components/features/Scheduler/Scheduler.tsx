d5d178774b2017ae67816d3dd01e957e360fd918
Leejoosub <joosublee@hotmail.com>
2020-07-30 23:50:35 -0400

    removed material ui container, and made my own

--- src/components/features/Scheduler/Scheduler.tsx
+++ src/components/features/Scheduler/Scheduler.tsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-//MATERIAL UI
-import Container from "@material-ui/core/Container";
-
 import styles from "./Scheduler.module.css";
 import { TopMenu } from "../../UI/TopMenu/TopMenu";
 import { WeekCalendar } from "../../UI/WeekCalendar/WeekCalendar";
@@ -11,12 +8,14 @@ import AddTask from "../AddTask/AddTask";
 
 export const Scheduler = () => {
   return (
-    <Container>
-      <TopMenu />
-      <Paper elevation={24} className={styles.calendar}>
-        <WeekCalendar />
-      </Paper>
-      <AddTask />
-    </Container>
+    <div className={styles.scheduleScreen}>
+      <div className={styles.scheduleContainer}>
+        <TopMenu />
+        <Paper elevation={24} className={styles.calendar}>
+          <WeekCalendar />
+        </Paper>
+        <AddTask />
+      </div>
+    </div>
   );
 };
